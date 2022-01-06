3d6de8efb1ec81084f4eeccec29b6f3443c2e50f
Aryan Sethi <aryan.rdps@gmail.com>
2022-01-06 23:33:59 +0530

    Global Styles

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Page</h2>
+            <button className='btn btn-primary'> Hello World </button>
+        </div>
+    )
+}
+
+export default About
