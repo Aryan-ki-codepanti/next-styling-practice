9d65550a23823982a77e484e20d44a6460f902c2
Aryan Sethi <aryan.rdps@gmail.com>
2022-01-13 22:51:32 +0530

    CSS in JS

--- pages/cssjs.js
+++ pages/cssjs.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+    font-size: 50px;
+    color: ${({ theme }) => theme.colors.primary};
+`;
 
 const CSSJS = () => {
     return (
         <div>
-            
+            <h1 style={{ color: "red" }}> Hello World </h1>
+            <Title>Styled Components</Title>
         </div>
-    )
-}
+    );
+};
 
-export default CSSJS
+export default CSSJS;
